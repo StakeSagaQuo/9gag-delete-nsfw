7bd146fc8961b07fb9a50ed3f40bd7eb74da1bde
StakeSagaQuo <stakesagaquo@gmail.com>
2015-02-18 09:56:36 -0500

    supports 9gag.tv; removes sidebar nsfw
    Adds support for 9gag.tv. That site does not have any NSFW filters built-in, so the script looks for videos with NSFW in the title (seems to be fairly consistent).

--- 9gag_nsfw_delete.user.js
+++ 9gag_nsfw_delete.user.js
@@ -4,7 +4,9 @@
 // @description Removes NSFW posts (9gag displays a click-to-show option).
 // @include     http://9gag.com/*
 // @include     http://9gag.com/
-// @version     1.1
+// @include     http://9gag.tv/*
+// @include     http://9gag.tv/
+// @version     1.2
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/StakeSagaQuo/9gag-delete-nsfw/master/9gag_nsfw_delete.user.js
 // @updateURL https://raw.githubusercontent.com/StakeSagaQuo/9gag-delete-nsfw/master/9gag_nsfw_delete.user.js
@@ -13,9 +15,16 @@
 jQuery( document ).ready(function(){
   // remove the nsfw links
   jQuery('a[href$="nsfw"]').parents('li').remove(); 
+  
+  // remove any sidebar items that contain NSFW in text (not the best way to do it, but they don't have a class).
+  // these don't get populated by scrolling, so no need to do it on ajax complete.
+  jQuery("li.badge-featured-item:contains('NSFW')").remove(); // sidebar links
+  
+  jQuery("div.badge-grid-item:contains('NSFW')").remove(); // 9gag.tv links. Almost all NSFW videos have this text.
 });
 jQuery( document ).ajaxComplete(function() {
   // remove new posts as you scroll
-  jQuery('.badge-nsfw-entry-cover, .nsfw-post').parents('article').remove(); // on ajax (scroll to bottom, initial page load), delete nsfw posts
+  
+  // on ajax (scroll to bottom, initial page load), delete nsfw posts
+  jQuery('.badge-nsfw-entry-cover, .nsfw-post').parents('article').remove(); 
 });
-
