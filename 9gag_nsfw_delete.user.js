18f79c9520041e37a1ab390ea6239e48ee312f69
StakeSagaQuo <stakesagaquo@gmail.com>
2015-02-18 10:02:25 -0500

    Update version; sidebar is ajax, so moved to correct function

--- 9gag_nsfw_delete.user.js
+++ 9gag_nsfw_delete.user.js
@@ -6,7 +6,7 @@
 // @include     http://9gag.com/
 // @include     http://9gag.tv/*
 // @include     http://9gag.tv/
-// @version     1.2.1
+// @version     1.2.2
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/StakeSagaQuo/9gag-delete-nsfw/master/9gag_nsfw_delete.user.js
 // @updateURL https://raw.githubusercontent.com/StakeSagaQuo/9gag-delete-nsfw/master/9gag_nsfw_delete.user.js
@@ -16,9 +16,7 @@ jQuery( document ).ready(function(){
   // remove the nsfw links
   jQuery('a[href$="nsfw"]').parents('li').remove(); 
   
-  // remove any sidebar items that contain NSFW in text (not the best way to do it, but they don't have a class).
-  // these don't get populated by scrolling, so no need to do it on ajax complete.
-  jQuery("li.badge-featured-item:contains('NSFW')").remove(); // sidebar links
+  
   
 });
 
@@ -26,6 +24,9 @@ jQuery( document ).ready(function(){
 jQuery( document ).ajaxComplete(function() {
   // on ajax (scroll to bottom, initial page load), delete nsfw posts
   jQuery('.badge-nsfw-entry-cover, .nsfw-post').parents('article').remove();   
+  
+  // remove any sidebar items that contain NSFW in text (not the best way to do it, but they don't have a class).
+  jQuery("li.badge-featured-item:contains('NSFW')").remove(); // sidebar links
   
   jQuery("div.badge-grid-item:contains('NSFW')").remove(); // 9gag.tv links. Almost all NSFW videos have this text.
 });
